9782b2731b3e051d83286c5aa2c33eb5f2a0a890
valencincu <83369023+valencincu@users.noreply.github.com>
2025-03-13 15:27:12 +0000

    Deploying to gh-pages from @ valencincu/test@1e351775f406ae41bb1370354fb40b22fe28cf32 🚀

---
+++ assets/js/fusejs/fusejs-store.js
@@ -0,0 +1,86 @@
+/*
+Copyright (c) 2013-2020 Michael Rose and contributors
+MIT License
+From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
+*/
+var store = [{
+        "title": "Archive",
+        "subtitle": "",
+        "excerpt": "",
+        "content": "",
+        "categories": null,
+        "date": null,
+        "tags": null,
+        "url": "https://dieghernan.github.io/test/archive",
+        "img": null
+      },{
+        "title": "Home",
+        "subtitle": "",
+        "excerpt": "<p>Talleres FIFA Bs As</p>",
+        "content": "Talleres FIFA Bs As              GitHub",
+        "categories": [],
+        "date": "2000-01-01",
+        "tags": [],
+        "url": "https://dieghernan.github.io/test/20000101_home/",
+        "img": null
+      },{
+        "title": "Python",
+        "subtitle": "",
+        "excerpt": "<p>Motivación</p>",
+        "content": "Motivación Programas Anaconda (descargar la versión +3.7). Spyder (viene con Anaconda) Jupyter notebook (viene con Anaconda) Guías del curso introductorio Guía 1: Introducción Python, variables, control de flujo, funciones y...",
+        "categories": [],
+        "date": "2000-01-02",
+        "tags": [],
+        "url": "https://dieghernan.github.io/test/20000102_python/",
+        "img": null
+      },{
+        "title": "LaTeX",
+        "subtitle": "",
+        "excerpt": "<p>Programas  MikTeX (Windows).  TeXLive (Linux).  Instalar con sudo apt-get install texlive-full. Si no funciona, en Ubuntu, se deben agregar los repositorios Universe.  TeXMaker.  Overleaft.</p>",
+        "content": "Programas MikTeX (Windows). TeXLive (Linux). Instalar con sudo apt-get install texlive-full. Si no funciona, en Ubuntu, se deben agregar los repositorios Universe. TeXMaker. Overleaft. Recursos WikiBooks Detexify y symbols-a4.pdf Editor...",
+        "categories": [],
+        "date": "2000-01-03",
+        "tags": [],
+        "url": "https://dieghernan.github.io/test/20000103_latex/",
+        "img": null
+      },{
+        "title": "Inscripción",
+        "subtitle": "",
+        "excerpt": "<p>Está lista la inscripción para este cuatrimestre!! Pueden realizarla AQUI. Por favor complétenla lo antes posible!!</p>",
+        "content": "Está lista la inscripción para este cuatrimestre!! Pueden realizarla AQUI. Por favor complétenla lo antes posible!!",
+        "categories": [],
+        "date": "2000-01-04",
+        "tags": [],
+        "url": "https://dieghernan.github.io/test/20000104_inscripcion/",
+        "img": null
+      },{
+        "title": "Nosotros",
+        "subtitle": "",
+        "excerpt": "<p>La FIFA (Federación Interestudiantil de Físicos Argentinos) es la federación a través de la cual los estudiantes de física de todo el país organizamos encuentros y actividades para discutir cuestiones gremiales, académicas o de política científica. También es el espacio que tenemos los estudiantes (muchas veces dispersos cada uno en sus estudios) para conocernos mejor los unos a los otros.</p>",
+        "content": "La FIFA (Federación Interestudiantil de Físicos Argentinos) es la federación a través de la cual los estudiantes de física de todo el país organizamos encuentros y actividades para discutir cuestiones...",
+        "categories": [],
+        "date": "2000-01-05",
+        "tags": [],
+        "url": "https://dieghernan.github.io/test/20000105_mas_info/",
+        "img": null
+      },{
+        "title": "Markdown cheatsheet",
+        "subtitle": "The basic syntax of your page",
+        "excerpt": "<p>A demo page checking Markdown</p>",
+        "content": "What’s Markdown? Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents. Created by John Gruber in 2004, Markdown is now one...",
+        "categories": ["tutorial"],
+        "date": "2025-03-13",
+        "tags": ["starter","syntax","markdown"],
+        "url": "https://dieghernan.github.io/test/cheatsheets/01-markdown-cheat-sheet",
+        "img": null
+      },{
+        "title": "kramdown cheatsheet",
+        "subtitle": "Beyond Markdown",
+        "excerpt": "<p>kramdown supercharges Markdown with some interesting features.</p>",
+        "content": "What’s kramdown? kramdown supercharges Markdown with some interesting features. kramdown is the default Jekyll Markdown processor. When creating your site with Jekyll, you can use the standard Markdown syntax plus...",
+        "categories": ["tutorial"],
+        "date": "2025-03-13",
+        "tags": ["starter","syntax","kramdown"],
+        "url": "https://dieghernan.github.io/test/cheatsheets/02-kramdown-cheat-sheet",
+        "img": null
+      }]
